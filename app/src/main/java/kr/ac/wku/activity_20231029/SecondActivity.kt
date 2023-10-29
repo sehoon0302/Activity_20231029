85c51e71a457e374a819e716b25fa6217c65e203
sehoon0302 <saehun66@gmail.com>
2023-10-29 09:55:35 +0900

    두번째 화면 별도로 데이터 바인딩 설정

--- app/src/main/java/kr/ac/wku/activity_20231029/SecondActivity.kt
+++ app/src/main/java/kr/ac/wku/activity_20231029/SecondActivity.kt
@@ -2,10 +2,14 @@ package kr.ac.wku.activity_20231029
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.wku.activity_20231029.databinding.ActivityMainBinding
+import kr.ac.wku.activity_20231029.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = DateBindingUtil.setContentView(this,R.layout.activity_second)
+
     }
 }
