04209299f7871e1ed5aacc78219cfca955c60ff2
sehoon0302 <saehun66@gmail.com>
2023-10-29 10:11:26 +0900

    3번화면 추가 - 메인에서 적은 문구를 들고 3번에 전달

--- app/src/main/java/kr/ac/wku/activity_20231029/ThirdActivity.kt
+++ app/src/main/java/kr/ac/wku/activity_20231029/ThirdActivity.kt
@@ -4,18 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import kr.ac.wku.activity_20231029.databinding.ActivitySecondBinding
+import kr.ac.wku.activity_20231029.databinding.ActivityThirdBinding
 
-class SecondActivity : AppCompatActivity() {
-    lateinit var binding: ActivitySecondBinding
+class ThirdActivity : AppCompatActivity() {
+    lateinit var binding: ActivityThirdBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_second)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_third)
 
-        binding.btnReturn.setOnClickListener {
-            //현재 화면 종료 : 밑에 깔린 이전화면이 드러남
-            //finish()
-
-
-        }
     }
 }
